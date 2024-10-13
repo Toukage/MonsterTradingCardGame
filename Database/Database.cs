39762829fc0fb8f9d542a3a01023d296e1f5f521
sura hameed <sura.hameed@gmx.at>
2024-10-13 13:48:08 +0200

    database connection to postgres db named MTCG_DB , which is handeld from a docker container, connected by using NPGSQL nuget package, connection is also opened right after making it , to be used anywhere its needed (for now in usermanagement and tokenmanager

---
+++ Database/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Npgsql;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardGame.Database
+{
+    public class Database
+    {
+        private static string connectionString = "Host=localhost;Port=5432;Username=toukage;Password=mtcgserver;Database=MTCG_DB";
+
+        public static NpgsqlConnection Connection()
+        {
+            NpgsqlConnection connection = new NpgsqlConnection(connectionString);//erstellt eine verbindung zur datenbank aus den infos von connectionString
+            connection.Open(); // opend die connection zur datenbank
+            return connection;
+        }
+    }
+}
