748f0fa669e4b6a305ba5bf38e38447f2d74a1f6
sura hameed <sura.hameed@gmx.at>
2024-10-13 13:56:45 +0200

    Model classes in DataLayer, not yet in use, alltough usermanager already has a cardmanager methode, will be moved at some point

---
+++ DataLayer/TradeManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardGame.DataLayer
+{
+    internal class TradeManager
+    {
+    }
+}
