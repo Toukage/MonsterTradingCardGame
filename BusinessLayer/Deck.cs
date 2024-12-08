23bab6d2728a82c6c79b139761f30140c4ccddea
sura hameed <sura.hameed@gmx.at>
2024-12-08 23:28:21 +0100

    Updated Decks

--- BusinessLayer/Deck.cs
+++ BusinessLayer/Deck.cs
@@ -9,6 +9,15 @@ namespace MonsterTradingCardGame.BusinessLayer
     public class Deck
     {
         public int UserId { get; set; }
-        public List<Card> Cards { get; set; } = new List<Card>();//to save cards, might change it to dictionary? probably not will see
+        public List<Card> Cards { get; set; } = new List<Card>();
+        public void CheckDeck(string body, string headers, StreamWriter writer)
+        {
+            //first check token
+            //then check if user has a deck -> if yes print
+            //if user doesnt have a deck, check if the body provides cards
+            //if yes CreateDeck
+            //if not 200 -> empty list
+        }
+
     }
 }
