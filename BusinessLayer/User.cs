20a37d1164ec9f727b157f6a07386030aa414d21
sura hameed <sura.hameed@gmx.at>
2024-12-08 23:37:17 +0100

    moved login and register from UserManager to User for better code structure

--- BusinessLayer/User.cs
+++ BusinessLayer/User.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MonsterTradingCardGame.DataLayer;
+using MonsterTradingCardGame.Routing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +8,58 @@ using System.Threading.Tasks;
 
 namespace MonsterTradingCardGame.BusinessLayer
 {
-    public class User//not in use yet
+    public class User
     {
+        private readonly Parser _parser = new();
+        private readonly Tokens _token = new();
+        private readonly Response _response = new();
+        private readonly UserManager _userMan = new();
+
         public int UserId { get; set; }
-        public string Username { get; set; }
+        public string UserName { get; set; }
         public string PasswordHash { get; set; }//passwords are not yet being hashed    
-        public int Coins { get; set; } = 20;//default 20 coins, but in db also specified to add to every generated user, so might change
-        public int Elo { get; set; } = 100;//default 100 elo, but in db also specified to add to every generated user, so might change
+        public int Coins { get; set; } 
+        public int Elo { get; set; } 
         public int GamesPlayed { get; set; }
         public int Wins { get; set; }
         public int Losses { get; set; }
+
+        //----------------------LOGIN----------------------
+        public void Login(string body, string headers, StreamWriter writer)
+        {
+            Console.WriteLine("** inside login function **");//debug
+
+            var (username, password) = _parser.UserDataParse(body, writer);
+
+            bool Valid = _userMan.GetUser(username, password);
+            if (Valid)
+            {
+                int? userId = _userMan.GetUserId(username);
+                Console.WriteLine($"** user id gotten : {userId} **");//debug
+                string token = _token.GetToken(userId.Value, username, writer);
+                Console.WriteLine($"** token gotten : {token} **");//debug
+            }
+            else
+            {
+                _response.HttpResponse(401, "Login Failed", writer);
+            }
+        }
+
+        //----------------------REGISTRATION----------------------
+        public void Register(string body, StreamWriter writer)
+        {
+            Console.WriteLine("** inside register **");//debug
+
+            var (username, password) = _parser.UserDataParse(body, writer);
+
+            Console.WriteLine($"** the credentioals inside the registr function name : {username}, pass : {password}  **");//debug
+
+            bool Valid = _userMan.InsertUser(username, password, writer);
+            if (Valid)
+            {
+                Console.WriteLine("** isnide valid if statement for register **");//debug
+                _response.HttpResponse(201, "Successfully Registered", writer);
+            }
+        }
     }
 }
