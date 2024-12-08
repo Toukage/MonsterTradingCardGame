06541390bd005c93679be2a021da0851420f4c7f
sura hameed <sura.hameed@gmx.at>
2024-12-08 23:32:53 +0100

    added path to hande deck action later on

--- DataLayer/DeckManager.cs
+++ DataLayer/DeckManager.cs
@@ -8,5 +8,17 @@ namespace MonsterTradingCardGame.DataLayer
 {
     internal class DeckManager
     {
+        private readonly Response _response;
+
+        public DeckManager()
+        {
+            _response = new Response();
+        }
+        public void GetDeck(StreamWriter writer)//get user deck
+        {
+            writer.WriteLine("GetDeck not yet Implemented");
+        }
     }
+
+    
 }
