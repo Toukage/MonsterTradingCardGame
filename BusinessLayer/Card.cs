b7ab7596c35c724538cc2230aef83a0f8d25e6cc
sura hameed <sura.hameed@gmx.at>
2024-12-09 00:06:12 +0100

    Updated Card class but no methods yet

--- BusinessLayer/Card.cs
+++ BusinessLayer/Card.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using MonsterTradingCardGame.DataLayer;
+using MonsterTradingCardGame.Routing;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +13,18 @@ namespace MonsterTradingCardGame.BusinessLayer
 {
     public class Card
     {
-        public string CardId { get; set; }
-        public string Name { get; set; }
-        public string CardType { get; set; }//monster or spell
-        public int Damage { get; set; }
-        public string ElementType { get; set; }//fire, water, earth, air etc
+        private readonly Response _response = new();
+        private readonly CardManager _cards = new();
+        private readonly Tokens _token = new();
+        [JsonProperty("Id")]
+        public string CardID { get; set; }
+        [JsonProperty("Name")]
+        public string CardName { get; set; }
+        [JsonProperty("Damage")]
+        public float CardDmg { get; set; }
+        
+        public string CardType { get; set; }
+        public string CardElement { get; set; }
+        public string CardMonster { get; set; }
     }
 }
